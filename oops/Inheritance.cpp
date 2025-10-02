0bf704553f5790d1495ea4d42ddb97c1c870317d
Sagar mittal <sagar@myillumine.com>
2025-10-02 17:53:37 +0530

    a

---
+++ oops/Inheritance.cpp
@@ -0,0 +1,11 @@
+// Learning Inheritance
+
+/*
+ Variables can be of 3 types, Private, Protected , Public
+ Private cannot be access by anyone
+ Procted only by child class
+ Public by anyone
+
+Access modifier
+Public Procted or Private Way
+*/
