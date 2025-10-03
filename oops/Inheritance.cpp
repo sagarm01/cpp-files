be8dbde654176a06926ed27363c354fe0849a46f
Sagar mittal <sagar@myillumine.com>
2025-10-03 10:15:01 +0530

    m

--- oops/Inheritance.cpp
+++ oops/Inheritance.cpp
@@ -9,3 +9,17 @@
 Access modifier
 Public Procted or Private Way
 */
+
+
+/*
+ Polymorphism
+ simply means many forms
+ same method can be defined in multi ways in different classes
+ and same method can behave differeny in same class based on situation
+ 1. Dynamic PM => same method can be defined in multi ways in different classes
+ 2. Static PM =>  and same method can behave differeny in same class based on situation
+
+ DP -> Method overriding -> handled by different implentation post inheritance by diff classes
+ SP -> Method Overloading -> handled by parameters -> same func defined multiple times with diff number of parameters
+ 
+*/
